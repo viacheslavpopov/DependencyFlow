e0cee4b26149d6d943764ba94dfeef80c78c8dcc
ilromali <iliashenkoroman@gmail.com>
2020-01-16 13:43:17 -0800

    Fix null-ref if build isn't found (#13)

--- Pages/Incoming.cshtml.cs
+++ Pages/Incoming.cshtml.cs
@@ -96,33 +96,26 @@ namespace DependencyFlow.Pages
 
         private async Task<(int?, DateTimeOffset)> GetCommitInfo(GitHubInfo gitHubInfo, Build build)
         {
-            var commitAge = DateTimeOffset.Now;
+            var commitAge = build.DateProduced;
             int? commitDistance = null;
-            if (gitHubInfo == null)
-            {
-                commitAge = build.DateProduced;
-            }
-            else
+            if (gitHubInfo != null)
             {
                 var comparison = await GetCommitsBehindAsync(gitHubInfo, build);
-                foreach (var commit in comparison.Commits)
+                if (comparison != null)
                 {
-                    if (commit.Commit.Committer.Date < commitAge)
+                    foreach (var commit in comparison.Commits)
                     {
-                        commitAge = commit.Commit.Committer.Date;
+                        if (commit.Commit.Committer.Date < commitAge)
+                        {
+                            commitAge = commit.Commit.Committer.Date;
+                        }
                     }
-                }
 
-                if (comparison.Commits.Count == 0)
-                {
-                    commitAge = build.DateProduced;
+                    // We're using the branch as the "head" so "ahead by" is actually how far the branch (i.e. "master") is
+                    // ahead of the commit. So it's also how far **behind** the commit is from the branch head.
+                    commitDistance = comparison.AheadBy;
                 }
-
-                // We're using the branch as the "head" so "ahead by" is actually how far the branch (i.e. "master") is
-                // ahead of the commit. So it's also how far **behind** the commit is from the branch head.
-                commitDistance = comparison.AheadBy;
             }
-
             return (commitDistance, commitAge);
         }
     }
