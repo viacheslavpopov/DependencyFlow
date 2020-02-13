cf06bb83b3f3bd45e0f805faeeb22e4ce21d2c49
Kevin Pilch <me@pilchie.com>
2020-02-13 14:07:37 -0800

    Add build information to footer

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +12,19 @@ namespace DependencyFlow
 {
     public class Program
     {
+        public static readonly string? SourceVersion;
+        public static readonly string? SourceBranch;
+
+        static Program()
+        {
+            var metadata = typeof(Program).Assembly
+                .GetCustomAttributes()
+                .OfType<AssemblyMetadataAttribute>()
+                .ToDictionary(m => m.Key, m => m.Value);
+            SourceVersion = metadata.TryGetValue("Build.SourceVersion", out var sourceVer) ? sourceVer : null;
+            SourceBranch = metadata.TryGetValue("Build.SourceBranch", out var sourceBranch) ? sourceBranch : null;
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
